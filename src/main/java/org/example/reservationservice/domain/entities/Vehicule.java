3bc8fe65c7eee02be17d5b5f6fa7b8aa81e0020a
kenz <kenzaworkspace@gmail.com>
2025-10-11 12:57:49 +0100

    new_entities

---
+++ src/main/java/org/example/reservationservice/domain/entities/Vehicule.java
@@ -0,0 +1,48 @@
+package org.example.reservationservice.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "vehicules")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vehicule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String licensePlate;
+
+    @Column(length = 50)
+    private String make;
+
+    @Column(length = 50)
+    private String model;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @OneToMany(mappedBy = "vehicule", cascade = CascadeType.ALL)
+    @Builder.Default
+    private Set<Reservation> reservations = new HashSet<>();
+}
